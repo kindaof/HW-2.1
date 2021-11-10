cc382349d5477e3971f54d39ec5dd2c94bbdb025
Dina <i.dina@bk.ru>
2021-11-10 21:34:26 +0300

    Initial Commit

---
+++ HW 2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.1
+//
+//  Created by Дина on 10.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
