86c3c24ae7f5de9bf2d8284a9719a4cdeb61e278
Dina <i.dina@bk.ru>
2021-11-14 19:36:07 +0300

    I added second part of HW 2.1

--- HW 2.1/ViewController.swift
+++ HW 2.1/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var yellowLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+   
+    @IBOutlet var button: UIButton!
+    
+    private var currentLight = CurrentLight.red
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        button.layer.cornerRadius = 10
+        button.setTitle("START", for: .normal)
+        
+        redLabel.alpha = 0.3
+        greenLabel.alpha = 0.3
+        yellowLabel.alpha = 0.3
+    }
+    // т.к. у меня все крадраты динамичны то использую метод viewWillLayoutSubviews если бы не зависили можно было бы перенести в viewDidLoad
+    
+    override func viewWillLayoutSubviews() {
+        redLabel.layer.cornerRadius = redLabel.frame.width / 2
+        yellowLabel.layer.cornerRadius = yellowLabel.frame.width / 2
+        greenLabel.layer.cornerRadius = greenLabel.frame.width / 2
     }
 
+    @IBAction func button(_ sender: UIButton) {
 
+        button.setTitle("NEXT", for: .normal)
+       
+        switch currentLight {
+        case .red:
+            greenLabel.alpha = 0.3
+            redLabel.alpha = 1
+            currentLight = .yellow
+        case .yellow:
+            redLabel.alpha = 0.3
+            yellowLabel.alpha = 1
+            currentLight = .green
+        case .green:
+            greenLabel.alpha = 1
+            yellowLabel.alpha = 0.3
+            currentLight = .red
+        }
+    }
 }
 
